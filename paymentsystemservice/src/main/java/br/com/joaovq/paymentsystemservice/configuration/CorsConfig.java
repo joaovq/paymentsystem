bbc570568d4b1390f25b8cf4d657b148ec9f967f
joaovq <joaovqdev@gmail.com>
2023-11-26 18:28:40 -0300

    feat: add config cors for api

---
+++ paymentsystemservice/src/main/java/br/com/joaovq/paymentsystemservice/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package br.com.joaovq.paymentsystemservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                var allowedOrigins = new String[]{
+                        "http://localhost:8080",
+                        "https://paymentsystem-pagnet-webapp.onrender.com"
+                };
+                registry.addMapping("/cnab").allowedOrigins(allowedOrigins);
+                registry.addMapping("/transactions").allowedOrigins(allowedOrigins);
+            }
+        };
+    }
+}
